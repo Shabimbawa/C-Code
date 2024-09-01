92da38d79d60ab94fcbd4a939eee0631a1fb55b3
Rhenz Largo <rhenzlargo80@gmail.com>
2024-09-01 21:45:12 +0800

    Added basic stack structure

--- helloworld.c
+++ helloworld.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+typedef struct{
+    int top=-1;
+    name customers[10];
+}stack;
 
+typedef char[10]
+
+typedef name char[50];
 int main(){
-    printf("hello world");
+    stack cList;
+
  }
+
+
+void push(stack * cList, name cust){
+
+}
+void pop(stack * cList){
+    if(cList->top > -1){
+        clist->top--;
+    }
+}
